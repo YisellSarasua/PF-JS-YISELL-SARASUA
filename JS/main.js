4d260346a0203d642c86fd3a7d9150a398a50599
Yisell Sarasua <yisell.sarasua@gmail.com>
2022-08-04 01:01:46 -0300

    agregue las funciones para la validacion de los datos

--- JS/main.js
+++ JS/main.js
@@ -17,17 +17,60 @@ window.addEventListener ('load', () => {
         const dataUsuario = usuario.value;
         const dataEmail = email.value;
         const dataPassword = password .value;
+
+        if(!dataNombre){
+
+            validacionErronea (nombre,'* Campo obligatorio');
+        } else {
+            console.log (dataNombre);
+            validacionCorrecta(nombre);
+        }
+
+        if(!dataUsuario){
+            validacionErronea (usuario,'* Campo obligatorio');
+        } else {
+            validacionCorrecta(usuario);
+        }
+
+        if(!dataEmail){
         
+            validacionErronea (email,'* Campo obligatorio');
+
+        } else if (!emailValido(dataEmail)) {
+            validacionErronea (email,'Email ingresado no es valido');
+
+        } else {
+            
+            validacionCorrecta(email);
+        }
 
 
+
+
+
+        
         console.log (dataNombre);
         console.log (dataUsuario);
         console.log (dataEmail);
-        console.log (dataPassword);
-        
-       
-        
+        console.log (dataPassword); 
 
+    }
+
+    function validacionErronea (input , mensaje) {
+        const validacionForm = input.parentElement;
+        const aviso = validacionForm.querySelector ('p');
+        aviso.innerText = mensaje;
+        validacionForm.className = 'mensaje falla';
+
+    }
+
+    function validacionCorrecta (input){
+        const validacionForm = input.parentElement;
+        validacionForm.className = 'mensaje falla';
+    }
+
+    function emailValido(email){
+        return /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/.test(email);
     }
 
 
